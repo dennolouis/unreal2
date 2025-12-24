9561e4a9e00438fbdc91a39434c072e8f831a040
Denno Louis <dennolouis1@gmail.com>
2025-12-24 18:52:53 -0500

    update to ue 5.7

--- Source/UnrealEditor.Target.cs
+++ Source/UnrealEditor.Target.cs
@@ -8,7 +8,7 @@ public class UnrealEditorTarget : TargetRules
 	public UnrealEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "Unreal" } );
 	}
